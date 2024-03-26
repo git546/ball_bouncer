906f8e083f27ad0bca88fa9504bb6212b69e87de
git546 <40750644+git546@users.noreply.github.com>
2024-03-27 08:59:57 +0900

    Create design_sets.py

---
+++ design_sets.py
@@ -0,0 +1,14 @@
+
+
+# 컬러 디자인 세트 정의
+color_design_sets = [
+    {'background_color': (255, 255, 255), 'ball_color': (255, 0, 0), 'border_color': (0, 0, 255)},
+    {'background_color': (0, 255, 255), 'ball_color': (255, 255, 0), 'border_color': (255, 0, 255)},
+    {'background_color': (255, 255, 0), 'ball_color': (0, 255, 255), 'border_color': (255, 0, 255)}
+]
+
+# 흑백 디자인 세트 정의
+monochrome_design_sets = [
+    {'background_color': (0, 0, 0), 'ball_color': (0, 0, 0), 'border_color': (255, 255, 255)},
+    {'background_color': (255, 255, 255), 'ball_color': (255, 255, 255), 'border_color': (0, 0, 0)},
+]
